55d44d4cb98dc6ac184ce203cc2f7e10cf7e88c6
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-03-19 15:00:00 +0100

    Added TodoItems component functionality

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -1,7 +1,13 @@
-function TodoItems({todos}) {
+import { CiCircleRemove } from 'react-icons/ci';
+
+function TodoItems(props) {
 	return (
-		<ul>
-			{todos.map((val, i) => <li key={i}>{val.text}</li>)}
+		<ul className="todo-items-container">
+			{props.todos.map((val, i) => 
+				<li className={'todo-item ' + val.addedClass} key={i}>
+					<p className="todo-item-text">{val.text}</p>
+					<button className={"item-delete-button " + val.addedClass} onClick={() => props.onClick(val.id)}><CiCircleRemove /></button>
+				</li>)}
 		</ul>
 	);
 }
